f0d2a8b3f2428f4ae1d01df15db1e11900316eec
Sebastian Castillo <sebastian.castillo.s@gmail.com>
2020-05-02 02:38:39 -0400

    useffect para cargar la primera frase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import Frase from "./components/Frase";
 
@@ -23,6 +23,12 @@ const Boton = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size 1s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
@@ -36,6 +42,11 @@ function App() {
     const frase = await api.json();
     guardarFrase(frase[0]);
   };
+
+  //cargar una frase al principio
+  useEffect(() => {
+    consultarApi();
+  }, []);
 
   return (
     <Contenedor>
